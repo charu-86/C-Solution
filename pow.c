58a792fc34729847ecd90a85ad98dc5a5552f881
Charu Kashyap <81952831+charu-86@users.noreply.github.com>
2022-10-13 00:38:19 +0530

    Added pow.c

---
+++ pow.c
@@ -0,0 +1,18 @@
+/* The popular pow method is something that everyon uses. But how does it actually work? Here's the background code for pow function */
+
+#include<stdio.h>
+
+double pow(int num, int p){
+  int res = 1;
+  while(n > 0){
+    if(n % 2 != 0)
+      res *= x;
+    x *= x;
+    n /=2;
+  }
+  return res;
+}
+
+int main(){
+  return 0;
+}
